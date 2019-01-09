4bd5c60fb61d268ad590f598569b1596f2282af3
Yoni Cohen <jonbir3@gmail.com>
2019-01-09 10:58:11 +0200

    create category model

---
+++ RoommatesExpensesManager/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RoommatesExpensesManager.Models
+{
+    public class Category
+    {
+        [Key]
+        public int cid { get; set; }
+        [Required (ErrorMessage = "שדה חובה")]
+        public string Type { get; set; }
+    }
+}
