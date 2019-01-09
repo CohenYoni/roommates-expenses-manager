ceebe53f2f22427680272f3de111f827c0c801e8
Yoni Cohen <jonbir3@gmail.com>
2019-01-09 10:57:33 +0200

    add category data access layer

---
+++ RoommatesExpensesManager/Dal/CategoryDal.cs
@@ -0,0 +1,19 @@
+﻿using RoommatesExpensesManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RoommatesExpensesManager.Dal
+{
+    public class CategoryDal : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().ToTable("category");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
