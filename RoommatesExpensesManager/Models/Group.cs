57984de694ef22624e7b329b2d5794807d8430d6
Yoni Cohen <jonbir3@gmail.com>
2019-01-10 14:13:38 +0200

    The schema has changed

--- RoommatesExpensesManager/Models/Group.cs
+++ RoommatesExpensesManager/Models/Group.cs
@@ -7,18 +7,23 @@ using System.Web;
 
 namespace RoommatesExpensesManager.Models
 {
-    //[Table("group")]
     public class Group
     {
-        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int gid { get; set; }
+        [Key]
+        [Column(Order = 2)]
         [Required (ErrorMessage = "שדה חובה")]
         public string city { get; set; }
+        [Key]
+        [Column(Order = 3)]
         [Required (ErrorMessage = "שדה חובה")]
         public string street { get; set; }
+        [Key]
+        [Column(Order = 4)]
         [Required(ErrorMessage = "שדה חובה")]
         [RegularExpression("^[0-9]+$", ErrorMessage = "מספר הדירה מורכב ממספרים בלבד")]
-        public int aptNum { get; set; }
+        public int? aptNum { get; set; }
         public string managerUserName { get; set; }
     }
 }
