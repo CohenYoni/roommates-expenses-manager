6814d7ba493895a94ae7833cbff09fa65f3b0064
orontr <34808128+orontr@users.noreply.github.com>
2019-01-10 16:25:11 +0200

    add expense dal and model

---
+++ RoommatesExpensesManager/Dal/ExpenseDal.cs
@@ -0,0 +1,19 @@
+﻿using RoommatesExpensesManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RoommatesExpensesManager.Dal
+{
+    public class ExpenseDal: DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Expense>().ToTable("expense");
+        }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ RoommatesExpensesManager/Models/Expense.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RoommatesExpensesManager.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int eid { get; set; }
+        
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "שדה חובה")]
+        public double? Amount { get; set; }
+        [Required(ErrorMessage = "שדה חובה")]
+        [StringLength(20, ErrorMessage = "לכל היותר 20 תווים")]
+        public string Store { get; set; }
+        [Required(ErrorMessage = "שדה חובה")]
+        public string Category { get; set; }
+        public string referanceNum { get; set; }
+        public string Comment { get; set; }
+        
+        public DateTime? expDate { get; set; }
+        
+    
+        public DateTime EntryDate { get; set; }
+       
+        public int groupID { get; set; }
+
+    }
+}
