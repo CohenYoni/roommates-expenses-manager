b0727b0783b20279aba0ad5f9e8d39c351f0b52a
orontr <34808128+orontr@users.noreply.github.com>
2019-01-10 16:26:00 +0200

    add expense view model

---
+++ RoommatesExpensesManager/ViewModel/VMExpense.cs
@@ -0,0 +1,20 @@
+﻿using RoommatesExpensesManager.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoommatesExpensesManager.ViewModel
+{
+    public class VMExpense
+    {
+        public int gid { get; set; }
+        public  Expense expense { get; set; }
+        public VMExpense(int g)
+        {
+            gid = g;
+            expense = new Expense();
+        }
+    }
+}
